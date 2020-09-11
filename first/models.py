c49105de61249b0b769cde3b39d7d05bb7271756
LeeJinbeom <jinbomang@gmail.com>
2020-09-11 18:10:24 +0900

    업로드기능

--- first/models.py
+++ first/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 from django import forms
+from uuid import uuid4
+import os
 
 def min_length_3(value):
     if len(value) < 3:
         raise forms.ValidationError("3글자 이상 입력해주세요!")
+
+def upload_to(instance, filename):
+    path = 'file'
+    uuid_name = uuid4().hex
+    #확장자 뽑는거
+    extension = os.path.splitext(filename)[-1].lower()
+    #filename.split('.')[-1]
+    return '/'.join([path, uuid_name + extension])
 
 # Create your models here.
 class Students(models.Model):
     name = models.CharField(max_length=10, validators=[min_length_3])
     address = models.CharField(max_length=50)
     email = models.CharField(max_length=30)
+    file1 = models.FileField(null=True, blank=True, upload_to='')
+    file2 = models.FileField(null=True, blank=True, upload_to=upload_to)
+    img1 = models.ImageField(null=True, blank=True, upload_to='images/')
+    img2 = models.ImageField(null=True, blank=True, upload_to='%Y/%m/%d')
 
 class Scores(models.Model):
     name = models.CharField(max_length=10)
